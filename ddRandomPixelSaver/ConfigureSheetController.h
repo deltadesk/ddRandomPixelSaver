c3814929f30bf5127b4f5f3f8b9931fd07e545c1
Andreas Meyer <objc@deltadesk.org>
2020-03-17 13:43:20 +0100

    Added ConfigureSheet

---
+++ ddRandomPixelSaver/ConfigureSheetController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigureSheetController.h
+//  ddRandomPixelSaver
+//
+//  Created by Andreas Meyer on 17.03.20.
+//  Copyright (c) 2020 DeltaDesk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ConfigureSheetController : NSWindowController
+
+@end
---
+++ ddRandomPixelSaver/ConfigureSheetController.m
@@ -0,0 +1,13 @@
+//
+//  ConfigureSheetController.m
+//  ddRandomPixelSaver
+//
+//  Created by Andreas Meyer on 17.03.20.
+//  Copyright (c) 2020 DeltaDesk. All rights reserved.
+//
+
+#import "ConfigureSheetController.h"
+
+@implementation ConfigureSheetController
+
+@end
